7aedc71d99ffb90abd781830f6ce859c1a8940ae
achraf hp <achrafelh22@gmail.com>
2019-08-17 11:21:22 +0100

    change the computer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils'
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       currentUser: null
-    }
+    };
   }
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      console.log(user)
-    })
+      createUserProfileDocument(user);
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
@@ -34,11 +33,11 @@ class App extends React.Component {
       <div>
         <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
-      </div >
+      </div>
     );
   }
 }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,23 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyBxt5pxU84oeNsgJcbLots5sEO765F1dCk",
-    authDomain: "crwn-clothing-e3422.firebaseapp.com",
-    databaseURL: "https://crwn-clothing-e3422.firebaseio.com",
-    projectId: "crwn-clothing-e3422",
-    storageBucket: "",
-    messagingSenderId: "31994910189",
-    appId: "1:31994910189:web:555ffb3bc95b3f6b"
+  apiKey: "AIzaSyBxt5pxU84oeNsgJcbLots5sEO765F1dCk",
+  authDomain: "crwn-clothing-e3422.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-e3422.firebaseio.com",
+  projectId: "crwn-clothing-e3422",
+  storageBucket: "",
+  messagingSenderId: "31994910189",
+  appId: "1:31994910189:web:555ffb3bc95b3f6b"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${uerAuth.uid}`);
+
+  const snapShot = await userRef.get();
 };
 
 firebase.initializeApp(config);
@@ -18,7 +26,9 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({
+  prompt: "select_account"
+});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
